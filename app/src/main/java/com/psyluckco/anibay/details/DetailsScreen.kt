928ad6a3491a3cd8e247ae6422b76753e5fae2a7
adarshshetty <adarsh.shetty@tismotech.com>
2025-04-09 18:41:34 +0530

    Commits made: - Details screen skeleton.

---
+++ app/src/main/java/com/psyluckco/anibay/details/DetailsScreen.kt
@@ -0,0 +1,17 @@
+/**
+ * Created by developer on 09-04-2025.
+ * Tismo Technology Solutions (P) Ltd.
+ * developers@tismotech.net
+ */
+
+package com.psyluckco.anibay.details
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DetailsScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
